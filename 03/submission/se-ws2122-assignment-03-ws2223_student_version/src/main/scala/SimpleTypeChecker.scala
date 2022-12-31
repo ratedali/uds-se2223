7aa58193f841fae63f0419e5a5f309efc2f7aa31
Ali Salaheldin <rated.ali.7@gmail.com>
2022-12-31 14:01:10 +0000

    implement task a

--- 03/submission/se-ws2122-assignment-03-ws2223_student_version/src/main/scala/SimpleTypeChecker.scala
+++ 03/submission/se-ws2122-assignment-03-ws2223_student_version/src/main/scala/SimpleTypeChecker.scala
@@ -37,9 +37,8 @@ case class TypeContext[IdT, TypeT] private (mapping: Map[IdT, TypeT]) {
 
 object TypeContext {
 
-  /** Magic function so that we can write 
-    * `TypeContext(("x", VType(BoolTy -> * A)))` instead of
-    * `new TypeContext(Map("x", VType(BoolTy -> A)))`.
+  /** Magic function so that we can write `TypeContext(("x", VType(BoolTy -> *
+    * A)))` instead of `new TypeContext(Map("x", VType(BoolTy -> A)))`.
     */
   def apply[IdT, TypeT](elems: (IdT, TypeT)*): TypeContext[IdT, TypeT] =
     new TypeContext(Map.from(elems))
@@ -68,6 +67,91 @@ object SimpleTypeChecker {
 class SimpleTypeChecker extends TypeChecker[Expression, Type, Context] {
 
   override def checkType(expr: Expression, context: Context): Result = {
-    // TODO: implement task a)
+    expr match {
+      case Const(True)   => Success(BoolTy)
+      case Const(False)  => Success(BoolTy)
+      case Const(Num(_)) => Success(NumTy)
+      case Id(id) =>
+        context.typeForVar(id) match {
+          case Some(t) => Success(t)
+          case None    => Failure(expr, context, s"Unknown identifier $id")
+        }
+      case Smaller(lhs, rhs) =>
+        (checkType(lhs, context), checkType(rhs, context)) match {
+          case (Success(NumTy), Success(NumTy)) =>
+            Success(BoolTy)
+          case (Success(NumTy), Success(rhsType)) =>
+            Failure(
+              expr,
+              context,
+              s"Type mismatch: expected $rhs to be NumTy, got $rhsType"
+            )
+          case (Success(lhsType), Success(_)) =>
+            Failure(
+              expr,
+              context,
+              s"Type mismatch: expected $lhs to be NumTy, got $lhsType"
+            )
+          case (Failure(_, _, msg), _) => Failure(expr, context, msg)
+          case (_, Failure(_, _, msg)) => Failure(expr, context, msg)
+        }
+      case If(cond, thenExpr, elseExpr) =>
+        (
+          checkType(cond, context),
+          checkType(thenExpr, context),
+          checkType(elseExpr, context)
+        ) match {
+          case (
+                Success(BoolTy),
+                Success(thenType),
+                Success(elseType)
+              ) if thenType == elseType =>
+            Success(thenType)
+          case (
+                Success(BoolTy),
+                Success(thenType),
+                Success(elseType)
+              ) =>
+            Failure(
+              expr,
+              context,
+              s"Type mismatch: expected both branches to have the same type, got $thenType and $elseType"
+            )
+          case (
+                Success(condType),
+                Success(_),
+                Success(_)
+              ) =>
+            Failure(
+              expr,
+              context,
+              s"Type mismatch: expected $cond to be BoolTy, got $condType"
+            )
+          case (Failure(_, _, msg), _, _) =>
+            Failure(expr, context, msg)
+          case (_, Failure(_, _, msg), _) =>
+            Failure(expr, context, msg)
+          case (_, _, Failure(_, _, msg)) =>
+            Failure(expr, context, msg)
+        }
+      case Let(variable, varValue, inExpr) => {
+        context.typeForVar(variable) match {
+          case Some(_) =>
+            Failure(
+              expr,
+              context,
+              s"$variable is already defined in this context"
+            )
+          case None => {
+            checkType(varValue, context) match {
+              case Success(varType) =>
+                checkType(inExpr, context.withVar(variable, varType))
+              case Failure(_, _, msg) => Failure(expr, context, msg)
+            }
+          }
+        }
+      }
+    }
+
   }
 }
